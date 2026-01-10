d8b24983adea895cf55f336b8c6ff4f9e48f7c49
Your Name <you@email.com>
2026-01-10 07:56:51 +0200

    Fix webm caching headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*.webm",
+        headers: [
+          { key: "Content-Type", value: "video/webm" },
+          // During active development, avoid caching headaches
+          { key: "Cache-Control", value: "no-store" },
+          { key: "Accept-Ranges", value: "bytes" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
